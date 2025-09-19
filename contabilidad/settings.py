26125e5ee19fc742cc84e6b75a2d31e837090e7a
AlexSolizDuran <solizelex1999@gmail.com>
2025-09-19 09:24:06 -0400

    pa render

--- contabilidad/settings.py
+++ contabilidad/settings.py
@@ -162,6 +162,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
+    "https://contabilidad-p2d4.onrender.com",
     "http://localhost:3000",  # tu frontend Next.js
     "https://mi-frontend.com",
 ]
